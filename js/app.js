6523013ad14f4c433148fee3c08112e456bf6af6
ZkunCry <aksentev.03@mail.ru>
2023-08-15 01:09:00 +0500

    redesigned event listener associated with adding and removing a class

--- js/app.js
+++ js/app.js
@@ -1,17 +1,13 @@
 const burgerButton = document.getElementById("burger");
 const header = document.querySelector(".header");
 const burgerBody = document.getElementById("burger-body");
+
 function removeOpen() {
   header.classList.remove("open");
-  console.log("Clicked");
 }
 
 burgerButton.addEventListener("click", () => {
-  if (header.classList.contains("open")) {
-    header.classList.remove("open");
-    burgerBody.removeEventListener("click", removeOpen);
-  } else {
-    header.classList.add("open");
-    burgerBody.addEventListener("click", removeOpen);
-  }
+  header.classList.toggle("open")
+    ? burgerBody.addEventListener("click", removeOpen)
+    : burgerBody.removeEventListener("click", removeOpen);
 });
